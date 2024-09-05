d06c05d0770276709c93b82f1e776b290e657bec
Stephane Kemgang <s.ialle.kemgang@accenture.com>
2024-09-05 15:30:16 +0400

    Initial comit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>My Backend API url is: {process.env.VITE_API_URL}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
