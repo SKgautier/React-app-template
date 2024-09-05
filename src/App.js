1573753f874b844e2864e0310913c66856dbe56a
Stephane Kemgang <s.ialle.kemgang@accenture.com>
2024-09-05 16:03:05 +0400

    Adding classic and dynamic env variables readings

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>My Backend API url is: {process.env.VITE_API_URL}</h1>
+        <h1>My Backend API url is: {window.__RUNTIME_CONFIG__.REACT_APP_API_URL}</h1>
+        <h1>My Backend API url is: {process.env.REACT_APP_API_URL}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
